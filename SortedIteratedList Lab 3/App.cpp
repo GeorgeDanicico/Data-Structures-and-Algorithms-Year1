fb47eb02825b3b0275ddc00fe1c9a1138a8a4a75
GeorgeDanicico <georgedanicico@yahoo.com>
2021-04-29 10:25:32 +0300

    up

--- SortedIteratedList Lab 3/App.cpp
+++ SortedIteratedList Lab 3/App.cpp
@@ -2,10 +2,12 @@
 
 #include "ShortTest.h"
 #include "ExtendedTest.h"
+#include "test_function.h"
 
 int main(){
     testAll();
     testAllExtended();
+    test_function();
     std::cout<<"Finished IL Tests!"<<std::endl;
 	system("pause");
 }
